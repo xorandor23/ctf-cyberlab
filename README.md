ae9518e656d2cc34f0ce20f77368766769d597aa
xornandnor <112608035+xornandor@users.noreply.github.com>
2025-09-14 00:41:08 +0700

    new push

--- README.md
+++
@@ -1,2 +0,0 @@
-# you-wouldn-t-get-this-from-any-other-guy-
-Hey! We just created a site about rock and roll. After you visit that site, check out this link!
---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const serverless = require("serverless-http");
+const path = require("path");
+const app = express();
+
+const flag = "HMTICTF{n3V3r_90nn4_9iV3_y0u_up_7b04bed1f671}"
+const link = "http://rocknroll.fun/"
+const PORT = 5000
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "./views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  if (req.get("Referer") == link) {
+    res.render('index', { flag });
+  } else {
+   res.render('welcome', { link });
+  }
+});
+
+/* module.exports = app; */
+/* module.exports.handler = serverless(app); */
+
+app.listen(PORT, () => {
+    console.log(`Challenge 1 Started`);
+    console.log(`Server running at http://localhost:${PORT}`);
+});
